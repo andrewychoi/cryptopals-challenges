18bc7834da1f422e03378e97d5eeec8551fca6cb
Andrew Choi <andrew@choi.com>
2018-08-30 20:57:17 -0400

    Add solution for challenge 9

---
+++ set2/challenge9.py
@@ -0,0 +1,39 @@
+def pkcs7_pad(plaintext, block_size):
+    """
+    Pad a plaintext to a specific block size.
+
+    Handles case where plaintext needs no padding
+
+    """
+    if len(plaintext) % block_size == 0:
+        return plaintext
+
+    multiple = len(plaintext) // block_size + 1
+
+    padded_length = multiple * block_size
+
+    missing = padded_length - len(plaintext)
+    pad_char = bytes([missing])
+
+    return plaintext + missing * pad_char
+
+
+def main():
+    testing_plaintext = b"YELLOW SUBMARINE"
+    output = b"YELLOW SUBMARINE\x04\x04\x04\x04"
+
+    to_check = pkcs7_pad(testing_plaintext, 20)
+    assert to_check == output
+
+    # make sure we don't error on an even multiple
+    assert pkcs7_pad(testing_plaintext, 16) == testing_plaintext
+
+    # make sure we don't fail on a smaller blocksize
+    assert pkcs7_pad(testing_plaintext, 5) == output
+
+    # make sure we don't fail on a larger blocksize
+    assert pkcs7_pad(testing_plaintext, 25) != testing_plaintext
+
+
+if __name__ == "__main__":
+    main()
