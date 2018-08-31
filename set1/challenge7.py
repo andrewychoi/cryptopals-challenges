10913bb5ce24aeedec954b212e7522526738c2a2
Andrew Choi <andrew@choi.com>
2018-08-31 12:29:56 -0400

    Add working solution with OpenSSL for challenge 7

--- set1/challenge7.py
+++ set1/challenge7.py
@@ -1,6 +1,17 @@
-import os
+import base64
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+
+
+def solve_openssl(ciphertext, key):
+    """
+    solution using openssl
+    """
+    backend = default_backend()
+    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=backend)
+    decryptor = cipher.decryptor()
+    decrypted = decryptor.update(ciphertext) + decryptor.finalize()
+    return decrypted
 
 
 def main():
@@ -8,18 +19,10 @@ def main():
     with open("7.txt") as infile:
         for line in infile:
             lines.append(line)
-    print(lines)
-    provided_key = "YELLOW SUBMARINE"
-    print(provided_key)
-
-    backend = default_backend()
-    key = os.urandom(32)
-    iv = os.urandom(16)
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-    encryptor = cipher.encryptor()
-    ct = encryptor.update(b"a secret message") + encryptor.finalize()
-    decryptor = cipher.decryptor()
-    decryptor.update(ct) + decryptor.finalize()
+    encoded_ciphertext = "".join(lines)
+    ciphertext = base64.b64decode(encoded_ciphertext)
+    provided_key = b"YELLOW SUBMARINE"
+    print(solve_openssl(ciphertext, provided_key))
 
 
 if __name__ == "__main__":
