bd4a3bf4c00aae6abc89737bf0a65a5136c96405
Andrew Choi <andrew@choi.com>
2018-09-03 23:37:43 -0400

    Reorganize directory structure
    The issue with the prior structure was that imports were being
    difficult. At some point in the future it's worth examining
    what the story is on Python packaging, but not for now.

--- challenge7.py
+++ challenge7.py
@@ -3,15 +3,27 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
 
-def solve_openssl(ciphertext, key):
-    """
-    solution using openssl
-    """
+def encrypt_aes_ecb(plaintext, key):
+    backend = default_backend()
+    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=backend)
+    encryptor = cipher.encryptor()
+    encrypted = encryptor.update(plaintext) + encryptor.finalize()
+    return encrypted
+
+
+def decrypt_aes_ecb(ciphertext, key):
     backend = default_backend()
     cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=backend)
     decryptor = cipher.decryptor()
     decrypted = decryptor.update(ciphertext) + decryptor.finalize()
     return decrypted
+
+
+def solve_openssl(ciphertext, key):
+    """
+    solution using openssl
+    """
+    return decrypt_aes_ecb(ciphertext, key)
 
 
 def main():
